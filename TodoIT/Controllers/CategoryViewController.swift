83adf9e8045df9f2604eb3db43b0e8adb6496d0f
inDave <indave@gmail.com>
2019-11-14 21:43:20 +0100

    Implementazione della funzione di ricerca e query con CoreData

---
+++ TodoIT/Controllers/CategoryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryViewController.swift
+//  TodoIT
+//
+//  Created by Davide Soncin on 14/11/2019.
+//  Copyright © 2019 Davide Soncin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView Datasource Methods
+
+
+    //MARK: - Data Manipulation Methods
+
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+        
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+
+}
